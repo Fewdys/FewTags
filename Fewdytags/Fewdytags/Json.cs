e552b73d74eda2d54967ee90f47e595eb8fa8d68
Edward7 <74219635+Edward7s@users.noreply.github.com>
2022-07-14 14:14:45 +0300

    Added a c# integration for the tags

---
+++ Fewdytags/Fewdytags/Json.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fewdytags
+{
+    internal class Json
+    {
+
+        [Serializable]
+        public class Tags
+        {
+            public List<Tag> records { get; set; }
+        }
+
+        public class Tag
+        {
+            public int id { get; set; }
+            public string UserID { get; set; }
+            public string Text { get; set; }
+        }
+    }
+}
---
+++ Fewdytags/Fewdytags/Plate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using System.Linq;
+namespace Fewdytags
+{
+    internal class Plate
+    {
+        public TMPro.TextMeshProUGUI Text { get; set; }
+        private GameObject _gameObject { get; set; }
+        private RectTransform[] _rectTransforms { get; set; }
+
+        ~Plate() { _rectTransforms = null; _gameObject = null; }
+        public Plate(VRC.Player player)
+        {
+            _gameObject = GameObject.Instantiate(player._vrcplayer.field_Public_PlayerNameplate_0.field_Public_GameObject_5, player._vrcplayer.field_Public_PlayerNameplate_0.field_Public_GameObject_0.transform).gameObject;
+            _gameObject.name = "Fewdy's Plate";
+            _rectTransforms = _gameObject.GetComponentsInChildren<RectTransform>().Where(x => x.name != "Trust Text" && x.name != "Fewdy's Plate").ToArray();
+            for (int i = 0; i < _rectTransforms.Length; i++)
+            {
+                try
+                {
+                    GameObject.DestroyImmediate(_rectTransforms[i].gameObject);
+                }
+                catch { }
+            }
+            _gameObject.SetActive(true);
+            _gameObject.transform.localPosition = new Vector3(0, -60, 0);
+            Text = _gameObject.transform.Find("Trust Text").gameObject.GetComponent<TMPro.TextMeshProUGUI>();
+            Text.text = "";
+        }
+
+    }
+}
